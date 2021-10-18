efe5aa9f673e08f04ba0018b5aad00c60b0f7314
迷渡 <justjavac@gmail.com>
2021-10-18 17:14:20 +0800

    add pref test
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,54 @@
+use std::fs;
+use std::time::Instant;
+
 use postcss::input::Input;
 use postcss::tokenizer::Tokenizer;
 
 fn main() {
-    // let input = Input::new("/* hello */\n.cls { font-size: 16px; }".to_string(), None);
-    let input = Input::new(" \"".to_string(), None);
+    let css = fs::read_to_string("./app.css").unwrap();
+    let input = Input::new(css, None);
+    let mut processor = Tokenizer::new(input, false);
+    while !processor.end_of_file() {
+        let start = Instant::now();
+        println!("{:?}: {:?}", processor.next_token(false), start.elapsed());
+    }
+
+    // let input = Input::new("/* hello */\n.cls { font-size: 16px; } .c {".to_string(), None);
+    let input = Input::new(":root,[data-color-mode=light][data-light-theme=light],[data-color-mode=dark][data-dark-theme=light]{/*! */}:root,[data-color-mode=light][data-light-theme=light],[data-color-mode=dark][data-dark-theme=light]{--color-canvas-default-transparent: rgba(255,255,255,0);--color-marketing-icon-primary: #218bff;--color-marketing-icon-secondary:".to_string(), None);
     let mut processor = Tokenizer::new(input, true);
+    let start = Instant::now();
     println!("{:?}", processor.next_token(false));
+    println!("time: {:?}", start.elapsed());
+
+    let start = Instant::now();
     println!("{:?}", processor.next_token(false));
+    println!("time: {:?}", start.elapsed());
+
+    let start = Instant::now();
     println!("{:?}", processor.next_token(false));
+    println!("time: {:?}", start.elapsed());
+
+    let start = Instant::now();
     println!("{:?}", processor.next_token(false));
+    println!("time: {:?}", start.elapsed());
+
+    let start = Instant::now();
     println!("{:?}", processor.next_token(false));
+    println!("time: {:?}", start.elapsed());
+
+    let start = Instant::now();
     println!("{:?}", processor.next_token(false));
+    println!("time: {:?}", start.elapsed());
+
+    let start = Instant::now();
     println!("{:?}", processor.next_token(false));
+    println!("time: {:?}", start.elapsed());
+
+    let start = Instant::now();
+    println!("{:?}", processor.next_token(false));
+    println!("time: {:?}", start.elapsed());
+
+    let start = Instant::now();
+    println!("{:?}", processor.next_token(false));
+    println!("time: {:?}", start.elapsed());
 }
