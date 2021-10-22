876e74eb87da43ab869aaf1d4029480851207a20
迷渡 <justjavac@gmail.com>
2021-10-22 08:46:42 +0800

    use twbs/bootstrap for bench
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ use postcss::input::Input;
 use postcss::tokenizer::Tokenizer;
 
 fn main() {
-  let css = fs::read_to_string("./app_big.css").unwrap();
-  let start = Instant::now();
+  let css = fs::read_to_string("assets/bootstrap.css").unwrap();
   let input = Input::new(css, None);
   let mut processor = Tokenizer::new(input, false);
+  let start = Instant::now();
   while !processor.end_of_file() {
     processor.next_token(false);
   }
   let end = start.elapsed();
-  println!("postcss-rs: {:?}", end);
+  println!("{:?}", end);
 
   // let input = Input::new("/* hello */\n.cls { font-size: 16px; } .c {".to_string(), None);
   // let mut processor = Tokenizer::new(input, true);
