85585d8e58d1ffe72261057cb9bac92e428d82aa
迷渡 <justjavac@gmail.com>
2021-11-02 10:35:40 +0800

    fix css type
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src/parse.rs
+++ src/parse.rs
@@ -2,7 +2,7 @@ use crate::input::{Input, ProcessOptions};
 use crate::parser::Parser;
 
 pub fn parse(css: &str, opts: Option<ProcessOptions>) {
-  let input = Input::new(css.to_owned(), opts);
+  let input = Input::new(css, opts);
   let mut parser = Parser::new(&input);
   parser.parse();
 }
