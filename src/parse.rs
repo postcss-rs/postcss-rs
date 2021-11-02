1001e7cb07df777472708225e92dd50e6dec35b6
迷渡 <justjavac@gmail.com>
2021-11-02 13:00:20 +0800

    add parse's return
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src/parse.rs
+++ src/parse.rs
@@ -1,8 +1,11 @@
 use crate::input::{Input, ProcessOptions};
+use crate::node::Node;
 use crate::parser::Parser;
+use std::cell::RefCell;
 
-pub fn parse(css: &str, opts: Option<ProcessOptions>) {
+pub fn parse(css: &str, opts: Option<ProcessOptions>) -> RefCell<Node> {
   let input = Input::new(css, opts);
-  let mut parser = Parser::new(&input);
+  let mut parser = Parser::new(input);
   parser.parse();
+  parser.root
 }
