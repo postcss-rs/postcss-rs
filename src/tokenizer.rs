609b522dde31f2d19f9503912fdb725292b66e6a
CGQAQ <m.jason.liu@outlook.com>
2021-11-02 17:55:57 +0800

    change base from 4 to 32

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -86,7 +86,7 @@ impl<'a> Tokenizer<'a> {
       length,
       pos: RefCell::new(0),
       // buffer: Vec::with_capacity(length / 13),
-      buffer: RefCell::new(AllocRingBuffer::with_capacity_power_of_2(4)),
+      buffer: RefCell::new(AllocRingBuffer::with_capacity_power_of_2(32)),
       returned: RefCell::new(Vec::with_capacity(min(MAX_BUFFER, length / 8))),
     }
   }
