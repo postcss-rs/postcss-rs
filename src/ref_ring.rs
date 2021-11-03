d16c56daeb23c49b4e6a93ed08ec0eae80aba8d5
CGQAQ <m.jason.liu@outlook.com>
2021-11-03 16:43:06 +0800

    Add SAFETY comment

--- src/ref_ring.rs
+++ src/ref_ring.rs
@@ -1,5 +1,4 @@
 use std::mem;
-
 const BUFFER_SIZE: usize = u8::MAX as usize + 1;
 
 #[derive(Debug)]
@@ -11,6 +10,8 @@ pub struct RefRing<'a> {
 impl<'a> Default for RefRing<'a> {
   fn default() -> Self {
     RefRing {
+      // SAFETY: SINCE NONE IS ZERO IN MEMORY REPRESENTATION,
+      // WE COULD JUST ZEROED OUT THE WHOLE BUFFER TO INITIALIZE IT.
       buffer: unsafe { mem::zeroed() },
       index: 0,
     }
@@ -20,14 +21,20 @@ impl<'a> Default for RefRing<'a> {
 impl<'a> RefRing<'a> {
   #[inline]
   pub fn push(&mut self, e: &'a str) {
+    // SAFETY: WE ARE TAKING ADVANTAGE OF UNSIGNED NUMBER OVERFLOW TO ELIMINATED BRANCHES
+    // AND IT'S GUARANTEED THAT INDEX IS ALWAYS IN BOUNDARY OF BUFFER, SO WE USE UNSAFE HERE TO
+    // BYPASSING RUST BOUNDARY CHECK.
     unsafe {
       (&mut *(self.buffer.as_mut_ptr().add(self.index as usize))).replace(e);
     }
-    self.index += 1;
+    self.index = self.index.wrapping_add(1);
   }
 
   pub fn pop(&mut self) -> Option<&'a str> {
-    self.index -= 1;
+    self.index = self.index.wrapping_sub(1);
+    // SAFETY: WE ARE TAKING ADVANTAGE OF UNSIGNED NUMBER OVERFLOW TO ELIMINATED BRANCHES
+    // AND IT'S GUARANTEED THAT INDEX IS ALWAYS IN BOUNDARY OF BUFFER, SO WE USE UNSAFE HERE TO
+    // BYPASSING RUST BOUNDARY CHECK.
     unsafe { (&mut *(self.buffer.as_mut_ptr().add(self.index as usize))).take() }
   }
 }
