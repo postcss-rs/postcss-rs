f3cbb95e27f4cda7fe69354856fd7efd9cd545ed
迷渡 <justjavac@gmail.com>
2021-11-11 09:16:27 +0800

    move `impl_node_traits` to macros.rs
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src/node.rs
+++ src/node.rs
@@ -1,5 +1,5 @@
-use crate::enum_mapping;
 use crate::input::Input;
+use crate::{enum_mapping, impl_node_traits};
 use serde::{Deserialize, Serialize};
 use std::cell::RefCell;
 use std::rc::{Rc, Weak};
@@ -203,36 +203,6 @@ pub trait NodeTrait<'a> {
   fn as_raws(&self) -> &dyn RawBefore;
   fn as_raws_mut(&mut self) -> &mut dyn RawBefore;
   fn as_trait(&'a self) -> &dyn NodeTrait<'a>;
-}
-
-macro_rules! impl_node_traits {
-  ($ident: ident) => {
-    impl<'a> NodeTrait<'a> for $ident<'a> {
-      fn get_nodes(&self) -> Option<Vec<Rc<RefCell<Node<'a>>>>> {
-        self.nodes.clone()
-      }
-
-      fn get_source(&self) -> Option<Source<'a>> {
-        self.source.clone()
-      }
-
-      fn set_source(&mut self, source: Source<'a>) {
-        self.source = Some(source);
-      }
-
-      fn as_raws(&self) -> &dyn RawBefore {
-        &self.raws as &dyn RawBefore
-      }
-
-      fn as_raws_mut(&mut self) -> &mut dyn RawBefore {
-        &mut self.raws as &mut dyn RawBefore
-      }
-
-      fn as_trait(&'a self) -> &dyn NodeTrait<'a> {
-        self as &dyn NodeTrait<'a>
-      }
-    }
-  };
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
