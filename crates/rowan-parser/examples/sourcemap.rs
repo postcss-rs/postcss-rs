40827c72e8377f35a1104f4da600944a3c485085
迷渡 <justjavac@gmail.com>
2021-11-18 15:50:50 +0800

    example: sourcemap (#62)
    * add sourcemap
    
    Signed-off-by: 迷渡 <justjavac@gmail.com>
    
    * print str
    
    Signed-off-by: 迷渡 <justjavac@gmail.com>
    
    * sourcemap
    
    Signed-off-by: 迷渡 <justjavac@gmail.com>

---
+++ crates/rowan-parser/examples/sourcemap.rs
@@ -0,0 +1,61 @@
+use rowan_parser::parser::Parser;
+use rowan_parser::syntax::SyntaxNode;
+use sourcemap::SourceMapBuilder;
+use std::time::Instant;
+
+use mimalloc_rust::*;
+
+#[global_allocator]
+static GLOBAL_MIMALLOC: GlobalMiMalloc = GlobalMiMalloc;
+fn main() {
+  let css = "#id { font-size: 12px; }";
+  // let css = include_str!("../../../assets/bootstrap.css");
+  let parser = Parser::new(css);
+  let parse = parser.parse();
+  let root = SyntaxNode::new_root(parse.green_node);
+
+  let start = Instant::now();
+  let mut smb = SourceMapBuilder::new(Some("stdin"));
+  let src_id = smb.add_source("stdin");
+  smb.set_source_contents(src_id, Some(css));
+  sourcemap(root, &mut smb, Some(src_id));
+  let sm = smb.into_sourcemap();
+  let mut output: Vec<u8> = vec![];
+  sm.to_writer(&mut output).unwrap();
+  println!("sourcemap\t{:?}", start.elapsed());
+  // println!("{}", std::str::from_utf8(&output).unwrap());
+
+  let start = Instant::now();
+  let mut smb = SourceMapBuilder::new(None);
+  let src_id = smb.add_source("stdin");
+  smb.set_source_contents(src_id, Some(css));
+  smb.add_raw(1, 0, 1, 0, Some(src_id), None);
+  smb.add_raw(1, 2, 1, 2, Some(src_id), None);
+  let sm = smb.into_sourcemap();
+  let mut output: Vec<u8> = vec![];
+  sm.to_writer(&mut output).unwrap();
+  println!("sourcemap\t{:?}", start.elapsed());
+  // println!("{}", std::str::from_utf8(&output).unwrap());
+}
+
+// postcss-js:                                        postcss-rs:
+// {                                                  {
+//   version: 3,                                        version: 3,
+//   sources: [ 'stdin' ],                              sources: [ "stdin" ],
+//   names: [],                                         names: [],
+//   mappings: 'AAAA,MAAM,eAAe,EAAE',                   mappings: ";AACA,GAAG,CAAC,CAAC,CAAC,SAAS,CAAC,CAAC,IAAI,CAAC,CAAC",
+//   file: 'stdin',                                     file: "stdin",
+//   sourcesContent: [ '#id { font-size: 12px; }' ]     sourcesContent: [ "#id { font-size: 12px; }" ]
+
+fn sourcemap(root: SyntaxNode, sb: &mut SourceMapBuilder, src_id: Option<u32>) {
+  root.preorder_with_tokens().for_each(|e| match e {
+    rowan::WalkEvent::Enter(n) => match n {
+      rowan::NodeOrToken::Node(_) => {}
+      rowan::NodeOrToken::Token(tok) => {
+        let col: u32 = tok.text_range().start().into();
+        sb.add_raw(1, col, 1, col, src_id, None);
+      }
+    },
+    rowan::WalkEvent::Leave(_) => {}
+  });
+}
