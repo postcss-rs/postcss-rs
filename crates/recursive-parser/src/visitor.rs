71714d11e9721b5dcdfa8d8cb100c7a3db66e607
IWANABETHATGUY <974153916@qq.com>
2021-11-21 23:44:28 +0800

    feat: 🎸 add life time

--- crates/recursive-parser/src/visitor.rs
+++ crates/recursive-parser/src/visitor.rs
@@ -1,15 +1,15 @@
 use crate::parser::{AtRule, Declaration, Root, Rule};
 
-pub trait Visit<R = ()> {
-  fn visit_root(&mut self, _root: &Root) -> R;
-  fn visit_rule(&mut self, _rule: &Rule) -> R;
-  fn visit_at_rule(&mut self, _at_rule: &AtRule) -> R;
-  fn visit_declaration(&mut self, _decl: &Declaration) -> R;
+pub trait Visit<'a, R = ()> {
+  fn visit_root(&mut self, _root: &Root<'a>) -> R;
+  fn visit_rule(&mut self, _rule: &Rule<'a>) -> R;
+  fn visit_at_rule(&mut self, _at_rule: &AtRule<'a>) -> R;
+  fn visit_declaration(&mut self, _decl: &Declaration<'a>) -> R;
 }
 
-pub trait VisitMut<R = ()> {
-  fn visit_root(&mut self, _root: &mut Root) -> R;
-  fn visit_rule(&mut self, _rule: &mut Rule) -> R;
-  fn visit_at_rule(&mut self, _at_rule: &mut AtRule) -> R;
-  fn visit_declaration(&mut self, _decl: &mut Declaration) -> R;
+pub trait VisitMut<'a, R = ()> {
+  fn visit_root(&mut self, _root: &mut Root<'a>) -> R;
+  fn visit_rule(&mut self, _rule: &mut Rule<'a>) -> R;
+  fn visit_at_rule(&mut self, _at_rule: &mut AtRule<'a>) -> R;
+  fn visit_declaration(&mut self, _decl: &mut Declaration<'a>) -> R;
 }
