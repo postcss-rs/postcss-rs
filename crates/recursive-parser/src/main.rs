d93ec4b52076f3967205211bfa80d4c0562a6b82
迷渡 <justjavac@gmail.com>
2021-11-30 09:16:05 +0800

    Parser bench (#101)
    * parser bench compare with postcss
    
    * update comparison data
    
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- crates/recursive-parser/src/main.rs
+++ crates/recursive-parser/src/main.rs
@@ -1,56 +1,26 @@
-#![feature(path_file_prefix)]
-use mimalloc_rust::*;
-use recursive_parser::parser::Parser;
-use recursive_parser::{parse, AstPrinter, WrapString};
+// use mimalloc_rust::*;
+use recursive_parser::parse;
+use std::fs::read_to_string;
 use std::time::Instant;
 
-#[global_allocator]
-static GLOBAL_MIMALLOC: GlobalMiMalloc = GlobalMiMalloc;
+// #[global_allocator]
+// static GLOBAL_MIMALLOC: GlobalMiMalloc = GlobalMiMalloc;
+
 fn main() {
-  let _str = include_str!("../../../assets/tailwind.css");
-  let _css = r#".test {
-      width: 100px
+  let file_list = [
+    ("tailwind-components.css", "2.8K"),
+    ("bootstrap-reboot.css", "7.4K"),
+    ("bootstrap-grid.css", "71K"),
+    ("bootstrap.css", "201K"),
+    ("tailwind.css", "3.5M"),
+    ("tailwind-dark.css", "5.8M"),
+  ];
+
+  for (file, size) in file_list {
+    let css: &str = &read_to_string(format!("../../assets/{}", file)).unwrap();
+    let start = Instant::now();
+    parse(css, Some(file));
+    let end = start.elapsed();
+    println!("rust: parser/{}({}): {:?}", file, size, end);
   }
-  @media {
-    .test result result{
-      height: 100px test test;
-    }
-    {
-      height: 100px;
-    }
-  }
-  "#;
-  let _css1 = r#":root {
-    --zero-size: {
-      width: 0;
-      height::"" 0;
-    };
-    --small-icon: {
-      width: 16px;
-      height: 16px;
-    }
-    ;
-  }"#;
-  let css2 = r#"
-a {
-
-  test test test
-
-
-"#;
-  // let tokens = tokenize(css2);
-  //  println!("{:#?}", tokens);
-
-  let start = Instant::now();
-  let parser = Parser::new(_str);
-  let _root = parser.parse().unwrap();
-  println!("{:?}", start.elapsed());
-  // let stdout = std::io::stdout();
-  // AstPrinter::new(0, stdout).print(&root).unwrap();
-
-  let root = parse(css2, None);
-  let mut printer = AstPrinter::new(0, WrapString::default());
-  printer.print(&root).unwrap();
-  let ast = printer.result().0;
-  print!("{}", ast);
 }
