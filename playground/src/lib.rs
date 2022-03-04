bf1e0c7c5f85afcaca230ec8d31964ce3e38faeb
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-03-04 12:52:37 +0800

    feat: 🎸 finish minimize

--- playground/src/lib.rs
+++ playground/src/lib.rs
@@ -23,3 +23,95 @@ pub fn ast(source: String) -> String {
     Err(_) => "".to_string(),
   }
 }
+
+#[wasm_bindgen]
+pub fn gen(source: String) -> String {
+  let parser = recursive_parser::parser::Parser::new(&source);
+
+  match parser.parse() {
+    Ok(mut root) => {
+      let mut minimize = MinimizePlugin::new(Vec::with_capacity(source.len()));
+      minimize.visit_root(&mut root).unwrap();
+      String::from_utf8(minimize.writer).unwrap_or_else(|_| "".to_string())
+    }
+    Err(_) => "".to_string(),
+  }
+}
+
+#[derive(Default)]
+struct MinimizePlugin<W: Write> {
+  level: usize,
+  writer: W,
+}
+
+impl<W: Write> MinimizePlugin<W> {
+  pub fn new(writer: W) -> Self {
+    Self { level: 0, writer }
+  }
+}
+
+impl<'a, W: std::io::Write> VisitMut<'a, std::io::Result<()>> for MinimizePlugin<W> {
+  fn visit_root(&mut self, root: &mut Root<'a>) -> std::io::Result<()> {
+    for child in root.children.iter_mut() {
+      match child {
+        RuleOrAtRuleOrDecl::Rule(rule) => {
+          self.visit_rule(rule)?;
+        }
+        RuleOrAtRuleOrDecl::AtRule(at_rule) => {
+          self.visit_at_rule(at_rule)?;
+        }
+        RuleOrAtRuleOrDecl::Declaration(_) => {
+          unreachable!()
+        }
+      }
+    }
+    Ok(())
+  }
+
+  fn visit_rule(&mut self, rule: &mut Rule<'a>) -> std::io::Result<()> {
+    self
+      .writer
+      .write(format!("{} {}", rule.selector.replace('\n', ""), "{").as_bytes())?;
+    self.level += 1;
+    for child in rule.children.iter_mut() {
+      match child {
+        RuleOrAtRuleOrDecl::Rule(_) => {
+          unreachable!()
+        }
+        RuleOrAtRuleOrDecl::AtRule(at_rule) => {
+          self.visit_at_rule(at_rule)?;
+        }
+        RuleOrAtRuleOrDecl::Declaration(decl) => {
+          self.visit_declaration(decl)?;
+        }
+      }
+    }
+    self.level -= 1;
+    write!(self.writer, "}}")?;
+    Ok(())
+  }
+
+  fn visit_at_rule(&mut self, at_rule: &mut AtRule<'a>) -> std::io::Result<()> {
+    write!(self.writer, "@{} {} {{", at_rule.name, at_rule.params)?;
+    self.level += 1;
+    for child in at_rule.children.iter_mut() {
+      match child {
+        RuleOrAtRuleOrDecl::Rule(rule) => {
+          self.visit_rule(rule)?;
+        }
+        RuleOrAtRuleOrDecl::AtRule(at_rule) => {
+          self.visit_at_rule(at_rule)?;
+        }
+        RuleOrAtRuleOrDecl::Declaration(_decl) => {
+          //   self.visit_declaration(decl);
+        }
+      }
+    }
+    self.level -= 1;
+    write!(self.writer, "}}")
+  }
+
+  fn visit_declaration(&mut self, decl: &mut Declaration<'a>) -> std::io::Result<()> {
+    write!(self.writer, "{}:{};", decl.prop, decl.value)
+  }
+}
--- playground/www/index.js
+++ playground/www/index.js
@@ -1,4 +1,4 @@
-import init, { ast } from "postcss-rs-wasm";
+import init, { ast, gen } from "postcss-rs-wasm";
 import { EditorState } from "@codemirror/next/state";
 import { css } from "@codemirror/next/lang-css";
 import { EditorView, basicSetup } from "@codemirror/next/basic-setup";
@@ -13,6 +13,7 @@ const code = `
 `.trim();
 (async () => {
   const interactiveAst = document.getElementById("interactive-ast");
+  const output = document.getElementById("output");
   await init();
   const languageTag = Symbol("language");
 
@@ -29,10 +30,13 @@ const code = `
     parent: document.querySelector("#editor"),
     dispatch: t => {
       editor.update([t]);
-      // console.log();
-      interactiveAst.innerHTML = `<pre>${ast(editor.state.doc.text.join("\n"))}</pre>`;
+      let sourceCode = editor.state.doc.text.join("\n");
+      output.innerHTML = gen(sourceCode);
+      interactiveAst.innerHTML = `<pre>${ast(sourceCode)}</pre>`;
     },
   });
 
-  interactiveAst.innerHTML = `<pre>${ast(editor.state.doc.text.join("\n"))}</pre>`;
+  let sourceCode = editor.state.doc.text.join("\n");
+  interactiveAst.innerHTML = `<pre>${ast(sourceCode)}</pre>`;
+  output.innerHTML = gen(sourceCode);
 })();
