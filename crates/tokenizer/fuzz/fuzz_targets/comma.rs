aebf5438eb4edb7515b35bbee60c5f239672c2b7
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-15 00:48:34 -0400

    fix module imports

--- crates/tokenizer/fuzz/fuzz_targets/comma.rs
+++ crates/tokenizer/fuzz/fuzz_targets/comma.rs
@@ -1,6 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use tokenizer::comma;
+use list::comma;
 
 fuzz_target!(|data: &[u8]| {
     let s = String::from_utf8_lossy(data);
--- crates/tokenizer/fuzz/fuzz_targets/space.rs
+++ crates/tokenizer/fuzz/fuzz_targets/space.rs
@@ -1,6 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use tokenizer::space;
+use list::space;
 
 fuzz_target!(|data: &[u8]| {
     let s = String::from_utf8_lossy(data);
