4b0aa65dcfb45161d60f14a37ac470a8e0dd1245
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-15 00:25:32 -0400

    fixed type error

--- crates/tokenizer/fuzz/fuzz_targets/tokenize.rs
+++ crates/tokenizer/fuzz/fuzz_targets/tokenize.rs
@@ -4,5 +4,6 @@ use libfuzzer_sys::fuzz_target;
 use tokenizer::tokenize;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = tokenize(&data);
+    let s = String::from_utf8_lossy(data);
+    let _ = tokenize(&s);
 });
