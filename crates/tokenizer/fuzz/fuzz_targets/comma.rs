b8a13b358e9bd8587306ece53688cf5a96d002cc
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-15 00:43:50 -0400

    new fuzzers

--- crates/tokenizer/fuzz/fuzz_targets/comma.rs
+++ crates/tokenizer/fuzz/fuzz_targets/comma.rs
@@ -1,8 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use tokenizer::tokenize;
+use tokenizer::comma;
 
 fuzz_target!(|data: &[u8]| {
     let s = String::from_utf8_lossy(data);
-    let _ = tokenize(&s);
+    let _ = comma(&s);
 });
--- crates/tokenizer/fuzz/fuzz_targets/space.rs
+++ crates/tokenizer/fuzz/fuzz_targets/space.rs
@@ -1,8 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use tokenizer::tokenize;
+use tokenizer::space;
 
 fuzz_target!(|data: &[u8]| {
     let s = String::from_utf8_lossy(data);
-    let _ = tokenize(&s);
+    let _ = space(&s);
 });
